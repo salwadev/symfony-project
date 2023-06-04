91c760fceeae8f8360d98dbe94c322dfc37cd0e5
Salwa <47577804+salwadev@users.noreply.github.com>
2023-06-04 03:36:18 +0200

    first commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+// src/Controller/HomeController.php
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/',methods:['GET'])]
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
+
+
+
+
+
